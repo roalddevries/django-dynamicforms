c7cd64aa11cbaf05fe52bbb700915066c7b8e5a6
Roald de Vries <git@roalddevries.nl>
2011-07-01 11:35:45 +0200

    corrected some typos, added a description, new version number

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name                 = 'django-dynamicforms',
-    version              = '0.1',
+    version              = '0.1.1',
     packages             = find_packages(),
     install_requires     = ['html2text'],
-    author               = 'Roald de Vries'
-    author_email         = 'roald@go2people.nl'
+    author               = 'Roald de Vries',
+    author_email         = 'roald@go2people.nl',
     include_package_data = True,
     url                  = 'https://github.com/roalddevries/django-dynamicforms/',
     license              = 'LICENSE.txt',
+    description          = 'Dynamically add forms to your site through the admin interface.',
     long_description     = open('README.rst').read(),
 )
